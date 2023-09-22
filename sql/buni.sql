c742ecb326f901a52dd4b0ec228a5f021a284de1
zp.wei <1348069215@qq.com>
2023-09-22 17:45:02 +0800

    perf: 更新sql

--- sql/buni.sql
+++ sql/buni.sql
@@ -11,18 +11,38 @@
  Target Server Version : 80016
  File Encoding         : 65001
 
- Date: 21/09/2023 17:27:16
+ Date: 22/09/2023 17:44:35
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
+-- Table structure for auth
+-- ----------------------------
+DROP TABLE IF EXISTS `auth`;
+CREATE TABLE `auth`  (
+  `id` bigint(20) NOT NULL COMMENT 'id',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',
+  `user_id` bigint(20) NULL DEFAULT NULL COMMENT '用户id',
+  `client_identity` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '客户端标识',
+  `token` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT 'token',
+  `can_use` tinyint(4) NULL DEFAULT 1 COMMENT '是否能使用(0:否，1:是)',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = '用户鉴权' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of auth
+-- ----------------------------
+INSERT INTO `auth` VALUES (1705150889788420098, '2023-09-22 17:23:47', '2023-09-22 17:40:19', 1704017394743595009, 'PostmanRuntime/7.32.3', 'vikJQq1WMhVYRZprd4i17WNvN2CLvKAS', 1);
+
+-- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-  `id` bigint(11) NOT NULL COMMENT 'id',
+  `id` bigint(20) NOT NULL COMMENT 'id',
   `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',
   `username` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '用户名',
@@ -33,7 +53,7 @@ CREATE TABLE `user`  (
   `is_admin` tinyint(4) NULL DEFAULT 0 COMMENT '是否是超级管理员(0:否，1：是)',
   `is_delete` tinyint(4) NULL DEFAULT 0 COMMENT '是否删除(0:否，1：是)',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = '用户' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
