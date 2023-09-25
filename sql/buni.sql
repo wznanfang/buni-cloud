b2d0a3794f8ac0266e027e14512ffd4a4fce2e6e
zp.wei <1348069215@qq.com>
2023-09-25 15:28:42 +0800

    feat: 添加角色权限

--- sql/buni.sql
+++ sql/buni.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80016
  File Encoding         : 65001
 
- Date: 22/09/2023 17:44:35
+ Date: 25/09/2023 15:28:31
 */
 
 SET NAMES utf8mb4;
@@ -35,7 +35,86 @@ CREATE TABLE `auth`  (
 -- ----------------------------
 -- Records of auth
 -- ----------------------------
-INSERT INTO `auth` VALUES (1705150889788420098, '2023-09-22 17:23:47', '2023-09-22 17:40:19', 1704017394743595009, 'PostmanRuntime/7.32.3', 'vikJQq1WMhVYRZprd4i17WNvN2CLvKAS', 1);
+INSERT INTO `auth` VALUES (1706122677032734721, '2023-09-25 09:45:19', '2023-09-25 15:26:54', 1704017394743595009, 'PostmanRuntime/7.33.0', 'tks9yhGn5VQ7m7Fnof1D71eesyGVmVqG', 1);
+
+-- ----------------------------
+-- Table structure for authority
+-- ----------------------------
+DROP TABLE IF EXISTS `authority`;
+CREATE TABLE `authority`  (
+  `id` bigint(20) NOT NULL COMMENT 'id',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '名字',
+  `parent_id` bigint(20) NULL DEFAULT NULL COMMENT '父级id',
+  `type` tinyint(4) NULL DEFAULT NULL COMMENT '0：模块，1：菜单，2：按钮',
+  `code` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '标识码',
+  `sort` tinyint(4) NULL DEFAULT NULL COMMENT '序号',
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '接口url',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = '角色' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of authority
+-- ----------------------------
+INSERT INTO `authority` VALUES (1, '2023-09-25 14:23:04', '2023-09-25 14:23:06', '根据id查询用户信息', 0, 2, 'user-details', 1, '/v1/findById/{id}/GET');
+
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role`  (
+  `id` bigint(20) NOT NULL COMMENT 'id',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '角色名字',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = '角色\r\n' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES (1, '2023-09-25 14:22:32', '2023-09-25 14:22:34', '管理员');
+
+-- ----------------------------
+-- Table structure for role_authority
+-- ----------------------------
+DROP TABLE IF EXISTS `role_authority`;
+CREATE TABLE `role_authority`  (
+  `id` bigint(20) NOT NULL COMMENT 'id',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',
+  `role_id` bigint(20) NULL DEFAULT NULL COMMENT '角色id',
+  `authority_id` bigint(20) NULL DEFAULT NULL COMMENT '权限id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = '角色权限' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of role_authority
+-- ----------------------------
+INSERT INTO `role_authority` VALUES (1, '2023-09-25 14:24:10', '2023-09-25 14:24:12', 1, 1);
+
+-- ----------------------------
+-- Table structure for sys_log
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_log`;
+CREATE TABLE `sys_log`  (
+  `id` bigint(20) NOT NULL COMMENT 'id',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',
+  `username` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '用户名',
+  `spendTime` bigint(20) NULL DEFAULT NULL,
+  `url` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `urlPath` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `method` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `ip` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `parameter` varchar(2550) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = '系统日志' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_log
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for user
@@ -59,5 +138,23 @@ CREATE TABLE `user`  (
 -- Records of user
 -- ----------------------------
 INSERT INTO `user` VALUES (1704017394743595009, '2023-09-19 14:19:42', '2023-09-19 14:19:42', '不逆', '123456', '不逆', 18, 1, 0, 0);
+
+-- ----------------------------
+-- Table structure for user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role`  (
+  `id` bigint(20) NOT NULL COMMENT 'id',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',
+  `user_id` bigint(20) NULL DEFAULT NULL COMMENT '用户id',
+  `role_id` bigint(20) NULL DEFAULT NULL COMMENT '角色id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = '用户角色' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_role
+-- ----------------------------
+INSERT INTO `user_role` VALUES (1, '2023-09-25 14:22:42', '2023-09-25 14:22:45', 1704017394743595009, 1);
 
 SET FOREIGN_KEY_CHECKS = 1;
