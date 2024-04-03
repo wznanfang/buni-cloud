374eef2387a57158004e1c330d85f3994bf7b5f9
zp.wei <wzp9215@qq.com>
2024-04-03 21:29:50 +0800

    perf: 添加注解

--- buni-user/buni-user-service/src/main/java/com/buni/userservice/UserServiceApplication.java
+++ buni-user/buni-user-service/src/main/java/com/buni/userservice/UserServiceApplication.java
@@ -4,10 +4,12 @@ package com.buni.userservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @MapperScan(value = "com.buni.userservice.mapper")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserServiceApplication {
 
