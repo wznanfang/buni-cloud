a900d93f0b2141777bb966459953bb698aeca689
zp.wei <zp.wei@zcbc.com>
2024-05-21 13:56:01 +0800

    feat: 新增网关限流

---
+++ buni-gateway/src/main/java/com/buni/gateway/config/IpAddressKeyResolver.java
@@ -0,0 +1,28 @@
+package com.buni.gateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author zp.wei
+ * @date 2024/5/21 13:17
+ */
+@Component
+public class IpAddressKeyResolver implements KeyResolver {
+
+
+    /**
+     * 根据ip地址限流
+     *
+     * @param exchange
+     * @return
+     */
+    @Override
+    public Mono<String> resolve(ServerWebExchange exchange) {
+        return Mono.just(exchange.getRequest().getRemoteAddress().getHostString());
+    }
+
+
+}
