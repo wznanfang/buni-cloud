2ef1d012bae8654bdf8cd298755660dfcb125def
不逆 <wzp9215@qq.com>
2025-02-25 20:49:51 +0800

    refactor(redis): 优化 RedisTemplate 序列化配置
    - 使用具体类替换 RedisSerializer 静态方法
    - 设置 key 和 value 的序列化方式为 String 和 JSON
    - 优化 hash 类型的 key 和 value 序列化方式

--- buni-framework/src/main/java/com/buni/framework/config/redis/RedisConfig.java
+++ buni-framework/src/main/java/com/buni/framework/config/redis/RedisConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * @author zp.wei
@@ -23,14 +25,14 @@ public class RedisConfig {
     public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory factory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(factory);
-        // 设置key序列化方式string，RedisSerializer.string() 等价于 new StringRedisSerializer()
-        redisTemplate.setKeySerializer(RedisSerializer.string());
-        // 设置value的序列化方式json，RedisSerializer.json() 等价于 new GenericJackson2JsonRedisSerializer()
-        redisTemplate.setValueSerializer(RedisSerializer.json());
+        // 设置key序列化方式string
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        // 设置value的序列化方式json
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         // 设置hash的key的序列化方式
-        redisTemplate.setHashKeySerializer(RedisSerializer.string());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         // 设置hash的value的序列化方式
-        redisTemplate.setHashValueSerializer(RedisSerializer.json());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
         // 使配置生效
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
