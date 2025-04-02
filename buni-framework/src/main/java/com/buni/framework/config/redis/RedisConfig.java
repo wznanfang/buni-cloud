d311b1f0d3669addf249fca61efc68e846ee06d8
wei <wzp9215@qq.com>
2025-04-02 16:58:11 +0800

    perf(redis): 优化 RedisTemplate序列化方式
    - 移除 ObjectMapper 的 DefaultTyping 配置，提高安全性
    - 使用默认的 GenericJackson2JsonRedisSerializer，简化配置
    - 调整代码格式，提高可读性

--- buni-framework/src/main/java/com/buni/framework/config/redis/RedisConfig.java
+++ buni-framework/src/main/java/com/buni/framework/config/redis/RedisConfig.java
@@ -1,9 +1,12 @@
 package com.buni.framework.config.redis;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -24,12 +27,9 @@ public class RedisConfig {
      */
     @Bean
     public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory factory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(factory);
-        ObjectMapper om = new ObjectMapper();
-        // 关键调整：使用更宽松的DefaultTyping级别
-        om.activateDefaultTyping(om.getPolymorphicTypeValidator(), ObjectMapper.DefaultTyping.EVERYTHING,JsonTypeInfo.As.PROPERTY);
-        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(om);
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer();
         redisTemplate.setKeySerializer(RedisSerializer.string());
         redisTemplate.setValueSerializer(serializer);
         redisTemplate.setHashKeySerializer(RedisSerializer.string());
